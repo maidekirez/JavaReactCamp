9b96f86d140afd6123003fd27e7807eb68687f5f
maidekirez <70860706+maidekirez@users.noreply.github.com>
2021-05-05 14:59:05 +0300

    Update BaseCustomerManager.java

--- homework2/Abstracts/BaseCustomerManager.java
+++ homework2/Abstracts/BaseCustomerManager.java
@@ -1,1 +1,15 @@
+package ınterfaceabstractdemo.Abstract;
+
+import ınterfaceabstractdemo.Entities.Customer;
+
+
+public abstract class BaseCustomerManager implements CustomerServiceInterface {
+    
+    @Override
+    public void save(Customer customer) {
+         System.out.println("saved to Db:  "+customer.getFirstName());
+    }
+    
+    
+}
 
