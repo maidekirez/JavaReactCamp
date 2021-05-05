a571e7f32a0a486a39098797ae29be6dc37d15cf
maidekirez <70860706+maidekirez@users.noreply.github.com>
2021-05-05 14:58:00 +0300

    Update Main.java

--- homework2/Main.java
+++ homework2/Main.java
@@ -1,1 +1,23 @@
 
+package ınterfaceabstractdemo;
+
+import Adapters.MernisServiceAdapters;
+import ınterfaceabstractdemo.Abstract.BaseCustomerManager;
+import ınterfaceabstractdemo.Concrete.NeroCustomerManager;
+import ınterfaceabstractdemo.Concrete.StarbucksCustomerManager;
+import ınterfaceabstractdemo.Entities.Customer;
+
+
+public class Main {
+    
+    public static void main(String[] args){
+        Customer customer1 = new Customer(2,"customer1","customer1","1998" ,"42898908570");
+
+        BaseCustomerManager baseCustomerManager = new NeroCustomerManager();
+        baseCustomerManager.save(customer1);
+        System.out.println();
+        
+        
+    }
+}
+
