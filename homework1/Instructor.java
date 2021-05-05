97147f29e9d77d721da750e5691e1a46172df86f
maidekirez <70860706+maidekirez@users.noreply.github.com>
2021-05-05 14:47:59 +0300

    Update Instructor.java

--- homework1/Instructor.java
+++ homework1/Instructor.java
@@ -1,1 +1,24 @@
+package javacamp2;
 
+
+public class Instructor extends User {
+    
+    double salary;
+    
+    
+    public Instructor(int id,String userNumber,String firstName, String lastName,double salary){
+        super(id,userNumber,firstName,lastName);
+        this.salary=salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    
+    
+}
